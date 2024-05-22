a65d7b687ac0b293fabfe6a93e9ffca95bc2f906
Jarfilled <manihemedani@gmail.com>
2024-05-22 19:02:47 +0330

    r

--- lib/sign_in/sign_in_page.dart
+++ lib/sign_in/sign_in_page.dart
@@ -1,4 +1,3 @@
-import 'package:Zerang/code_assets.dart/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -13,7 +12,10 @@ class _SignInPageState extends State<SignInPage> {
   final TextEditingController _weightController = TextEditingController();
   final TextEditingController _heightController = TextEditingController();
   String _userInfo = "";
-  String? _errorText;
+  String? _nameErrorText;
+  String? _emailErrorText;
+  String? _weightErrorText;
+  String? _heightErrorText;
 
   @override
   void dispose() {
@@ -25,38 +27,44 @@ class _SignInPageState extends State<SignInPage> {
   }
 
   bool _validateInput() {
+    bool isValid = true;
+    setState(() {
+      _nameErrorText = null;
+      _emailErrorText = null;
+      _weightErrorText = null;
+      _heightErrorText = null;
+    });
+
     final String name = _nameController.text;
     final String weight = _weightController.text;
     final String height = _heightController.text;
     final String email = _emailController.text;
+
     if (name.isEmpty) {
       setState(() {
-        _errorText = "لطفا نام خود را وارد کنید";
+        _nameErrorText = "لطفا نام خود را وارد کنید";
       });
-      return false;
+      isValid = false;
     }
     if (email.isEmpty) {
       setState(() {
-        _errorText = "لطفا پست الکترونیک خود را وارد کنید";
+        _emailErrorText = "لطفا پست الکترونیک خود را وارد کنید";
       });
-      return false;
+      isValid = false;
     }
     if (weight.isEmpty || int.tryParse(weight) == null) {
       setState(() {
-        _errorText = "وزن باید به صورت عدد وارد شود";
+        _weightErrorText = "وزن باید به صورت عدد وارد شود";
       });
-      return false;
+      isValid = false;
     }
     if (height.isEmpty || int.tryParse(height) == null) {
       setState(() {
-        _errorText = "قد باید به صورت عدد وارد شود";
+        _heightErrorText = "قد باید به صورت عدد وارد شود";
       });
-      return false;
+      isValid = false;
     }
-    setState(() {
-      _errorText = null;
-    });
-    return true;
+    return isValid;
   }
 
   void _signIn() {
@@ -75,82 +83,118 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ثبت نام', style: TextStyle(fontFamily: 'Vazir')),
-        centerTitle: true,
-      ),
-      body: Directionality(
-        textDirection: TextDirection.rtl,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'پست الکترونیک خود را وارد کنید',
-                  border: OutlineInputBorder(),
-                  errorText: _errorText,
-                ),
-                textAlign: TextAlign.right,
-              ),
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'نام کاربری خود را وارد کنید',
-                  border: OutlineInputBorder(),
-                  errorText: _errorText,
-                ),
-                textAlign: TextAlign.right,
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _weightController,
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: InputDecoration(
-                  labelText: 'وزن خود را وارد کنید (کیلوگرم)',
-                  border: OutlineInputBorder(),
-                  errorText: _errorText,
-                ),
-                textAlign: TextAlign.right,
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _heightController,
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: InputDecoration(
-                  labelText: 'قد خود را وارد کنید (سانتی‌متر)',
-                  border: OutlineInputBorder(),
-                  errorText: _errorText,
-                ),
-                textAlign: TextAlign.right,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _signIn,
-                child: Text('ثبت نام', style: TextStyle(fontFamily: 'Vazir', color: Colors.white)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-              ),
-              SizedBox(height: 20),
-              if (_userInfo.isNotEmpty)
-                Text(
-                  _userInfo,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: button_color,
-                    fontFamily: 'Vazir',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.lightBlueAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  SizedBox(height: 80),
+                  Center(
+                    child: Text(
+                      'ثبت نام',
+                      style: TextStyle(
+                        fontFamily: 'Vazir',
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  textAlign: TextAlign.center,
-                ),
-            ],
+                  SizedBox(height: 20),
+                  _buildTextField(
+                    controller: _emailController,
+                    label: 'پست الکترونیک خود را وارد کنید',
+                    errorText: _emailErrorText,
+                  ),
+                  SizedBox(height: 20),
+                  _buildTextField(
+                    controller: _nameController,
+                    label: 'نام کاربری خود را وارد کنید',
+                    errorText: _nameErrorText,
+                  ),
+                  SizedBox(height: 20),
+                  _buildTextField(
+                    controller: _weightController,
+                    label: 'وزن خود را وارد کنید (کیلوگرم)',
+                    errorText: _weightErrorText,
+                    inputType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  ),
+                  SizedBox(height: 20),
+                  _buildTextField(
+                    controller: _heightController,
+                    label: 'قد خود را وارد کنید (سانتی‌متر)',
+                    errorText: _heightErrorText,
+                    inputType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  ),
+                  SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: _signIn,
+                    child: Text('ثبت نام', style: TextStyle(fontFamily: 'Vazir', color: Colors.white)),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      backgroundColor: Colors.deepPurple,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  if (_userInfo.isNotEmpty)
+                    Text(
+                      _userInfo,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontFamily: 'Vazir',
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    String? errorText,
+    TextInputType inputType = TextInputType.text,
+    List<TextInputFormatter>? inputFormatters,
+  }) {
+    return TextField(
+      controller: controller,
+      keyboardType: inputType,
+      inputFormatters: inputFormatters,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.white),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.3),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+        errorText: errorText,
+      ),
+      style: TextStyle(color: Colors.white),
+      textAlign: TextAlign.right,
     );
   }
 }
